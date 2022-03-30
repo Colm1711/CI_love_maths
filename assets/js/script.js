b6e9d4c888ceb70549c4b5de264a8ab91c7b9d2d
Colm1711 <cosullivan1711@gmail.com>
2022-03-30 10:36:55 +0000

    Add structure for js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
-console.log('Hello there')
+//rungame
+
+function runGame(){
+
+}
+
+//check users answer
+function checkAnswer(){
+
+}
+
+//calculate if the answer is correct or incorrect
+function calculateCorrectAnswer(){
+
+}
+
+//increment the users score
+function incrementScore(){
+
+}
+
+//increment the wrong answer
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+    
+}
+
+
+function displayMultiplyQuestion(){
+    
+}
+
