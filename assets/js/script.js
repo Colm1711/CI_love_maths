d7a50f3e95c7d35f3ac968090fc4f082db5fa54a
Colm1711 <cosullivan1711@gmail.com>
2022-04-01 15:44:00 +0000

    fixed spelling of displayDivisionQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ function runGame(gameType){
         displaySubtractQuestion(num1, num2);
     }
     else if(gameType === "division"){
-        displayDivisonQuestion((num1 * num2), num2);
+        displayDivisionQuestion((num1 * num2), num2);
     }else{
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -167,7 +167,7 @@ function displayMultiplyQuestion(operand1, operand2){
 }
 
 //#Challenge
-function displayDivisonQuestion(operand1, operand2){
+function displayDivisionQuestion(operand1, operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "/";
